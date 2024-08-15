6d671bc3dabaa606c5f9a8f3955a8797c5e6254c
Shubham <gaikwadshubham65@gmail.com>
2024-08-15 22:53:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import { response } from './data';
+
+const express=require("express");
+const app=express();
+const dotenv=require("dotenv");
+const axios = require('axios');
+dotenv.config({path:'config.env'});
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.status(200).send("welcome to currency convertor");
+});
+
+app.get("/convert",async (req,res)=>{
+    const { from, to, amount } = req.query;
+    try{
+        //const apiKey = process.env.EXCHANGE_RATES_API_KEY;
+        //const response = await axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=${apiKey}`);
+        //rate=response.data.rates[to];
+        rate=response.rates[to];
+        convertedAmount=amount * rate;
+        res.status(200).json({ from, to, amount, rate, convertedAmount });
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching currency rate', details: error.message });
+    }
+});
+
+const PORT=5000;
+app.listen(PORT,()=>{
+        console.log(`server is running on:${PORT}`);
+})
